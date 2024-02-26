b3be8d10fad90b8dc54001611b6d507a16134fda
Noah Gram <noah@gramonline.nl>
2024-02-26 15:12:09 +0100

    Update bingo.py

--- week_3/sorting_algoritm/bingo.py
+++ week_3/sorting_algoritm/bingo.py
@@ -3,13 +3,17 @@ def bingo_sort_3_arrays(arr1, arr2, arr3):
         max_val = max(arr)
         min_val = min(arr)
         size = len(arr)
+        # Create the buckets
         buckets = [0] * (max_val - min_val + 1)
 
+        # Fill the buckets
         for num in arr:
             buckets[num - min_val] += 1
 
+        # Fill the sorted array
         sorted_arr = []
         for i in range(len(buckets)):
+            # Add the number of times the bucket's index appears in the array
             sorted_arr.extend([i + min_val] * buckets[i])
 
         return sorted_arr
@@ -25,6 +29,7 @@ arr1 = [64, 34, 25, 12, 22, 11, 90, 54, 67, 29, 10, 43, 88, 91, 100, 23, 36, 48,
 arr2 = [43, 21, 65, 90, 76, 88, 47, 33, 22, 99, 11, 7, 29, 85, 62, 50, 39, 20, 5, 74]
 arr3 = [53, 36, 29, 41, 15, 77, 82, 19, 63, 48, 90, 22, 60, 10, 84, 35, 45, 75, 56, 38]
 
+print("\n ( Bingo Sort ) \n")
 print("Original arrays:")
 print("Array 1:", arr1)
 print("Array 2:", arr2)
